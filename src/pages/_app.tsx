beba22e25b55160a2db1425e6b2948f23004e65f
Youhei Sugiyama <youhei-s@kpnet.co.jp>
2022-01-18 16:39:15 +0900

    first

---
+++ src/pages/_app.tsx
@@ -0,0 +1,31 @@
+import "../styles/globals.css";
+import type { AppProps } from 'next/app'
+import { FC } from 'react'
+import { AuthProvider, useAuth } from '../hooks/auth'
+
+
+const Layout: FC = ({ children }) => {
+  const { initialized, loggedIn, login } = useAuth();
+
+  if (!initialized) {
+    return <p>loading...</p>;
+  }
+
+  if (!loggedIn) {
+    return <button onClick={login}>log in</button>;
+  }
+
+  return <>{children}</>;
+};
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <AuthProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </AuthProvider>
+  );
+}
+
+export default MyApp;
