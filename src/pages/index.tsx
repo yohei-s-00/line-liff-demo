1b20832fa71c735e621b1139d1ac65ef456bba04
Youhei Sugiyama <youhei-s@kpnet.co.jp>
2022-01-18 16:52:18 +0900

    third

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,22 @@
 import Head from "next/head";
 import Link from 'next/link'
+import liff from "@line/liff";
 // import { useEffect, useState } from "react";
 import { useAuth } from "../hooks/auth";
+import { useState } from "react";
 
 export default function Home() {
+  const [value,setValue] = useState('');
   const { loggedIn, userId } = useAuth();
+  const handleClick = () => {
+    liff.scanCodeV2()
+    .then((result) => {
+      setValue(result.value);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+  }
 
   return (
     <div>
@@ -20,6 +32,12 @@ export default function Home() {
         </p>
         <p>
           <Link href="/sub">sub page</Link>
+        </p>
+        <p>
+          <button onClick={handleClick}>二次元コードリーダーを表示する</button>
+        </p>
+        <p>
+          {value}
         </p>
       </main>
     </div>
